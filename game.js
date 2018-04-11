e764a8c329f32b9904a965c4087c1ea698423c0e
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-04-11 14:40:53 -0700

    working on tictactoe

---
+++ game.js
@@ -0,0 +1,29 @@
+// create board
+// play X
+// play O
+// find winner
+// find draw
+// display player's turn
+// restart game
+
+const Game = () => {
+
+  const obj = {};
+
+  obj.board = [['', '', ''], ['', '', ''], ['', '', '']];
+  obj.playerX = 'X';
+  obj.playerO = 'O';
+
+  obj.playX = (row, column) => {
+
+  };
+
+  obj.playO = (row, column) => {
+
+  };
+
+  return obj;
+
+};
+
+console.log(Game());
